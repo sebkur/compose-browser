7ff6b692f9788f62915d2a4cc969c6b3211a5449
Sebastian Kürten <sebastian@topobyte.de>
2023-09-09 09:55:42 +0200

    Only follow links when clicked with first mouse button
    Otherwise clicking links using back/forward buttons leads to strange
    visit site and go back directly afterwards behavior.

--- compose/src/commonMain/kotlin/de/mobanisto/compose/browser/HtmlText.kt
+++ compose/src/commonMain/kotlin/de/mobanisto/compose/browser/HtmlText.kt
@@ -28,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.awt.awtEventOrNull
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.drawscope.DrawScope
@@ -369,7 +370,9 @@ fun HtmlText(
             // workaround for https://github.com/JetBrains/compose-multiplatform/issues/1450
             // todo: when fixed, change to ClickableText and move logic to onClick parameter
             .onPointerEvent(PointerEventType.Release) { event ->
-                processLink(event) { it?.let { handleLink(it) } }
+                if (event.awtEventOrNull?.button == 1) {
+                    processLink(event) { it?.let { handleLink(it) } }
+                }
             }.onPointerEvent(PointerEventType.Move) { event ->
                 processLink(event) { hoverLink(it) }
             },
